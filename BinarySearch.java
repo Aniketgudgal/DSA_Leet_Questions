d1a0ae2d0223c11726653b43e510a54f3f7480c3
Aniketgudgal <aniketgudgal5867@gmail.com>
2025-09-26 00:35:02 +0530

    binary search algorithm

--- BinarySearch.java
+++ BinarySearch.java
@@ -1,6 +1,7 @@
 class Solution {
     public int search(int[] nums, int target) {
-        // using brute force // time complexity is O(n)
+        // using brute force
+        /*
         int n= -1;
         for(int i = 0; i < nums.length; i++)
         {
@@ -11,5 +12,26 @@ class Solution {
             }
         }
         return n;
+        */
+        //using binary search
+        int start = 0; 
+        int end = nums.length - 1;
+        while(start <= end)
+        {
+            int mid = (start + end)/2;
+            if(target == nums[mid])
+            {
+                return mid;
+            }
+            if(target > nums[mid])
+            {
+                start = mid + 1;
+            }
+            else if(target < nums[mid])
+            {
+                end = mid - 1;
+            }
+        }
+        return -1;
     }
 }
